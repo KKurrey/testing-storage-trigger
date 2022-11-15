045e0816f72f9a1d82bbbef633f4cda86a78e56d
Kranti Kumari <kranti.kumari@ad1.searceinc.it>
2022-11-15 12:55:21 +0530

    Using GCP Bucket

--- src/main/java/academy/learnprogramming/hellomaven/StorageTrigger.java
+++ src/main/java/academy/learnprogramming/hellomaven/StorageTrigger.java
@@ -3,19 +3,21 @@ import com.google.cloud.functions.BackgroundFunction;
 import com.google.cloud.functions.Context;
 import java.util.logging.Logger;
 
-public class StorageTrigger implements BackgroundFunction<GCSEvent> {
+import com.google.cloud.storage.Bucket;
+
+public class StorageTrigger implements BackgroundFunction<Bucket> {
     private static final Logger logger = Logger.getLogger(StorageTrigger.class.getName());
 
     @Override
-    public void accept(GCSEvent event, Context context) {
-        logger.info("Processing file: " + event.name);
+    public void accept(Bucket event, Context context) {
+        logger.info("Processing file: " + event.getName());
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
 
 }
-class GCSEvent {
-    String bucket;
-    String name;
-    String metageneration;
-}
+//class GCSEvent {
+//    String bucket;
+//    String name;
+//    String metageneration;
+//}
